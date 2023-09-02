476c2f04048efaf3652685b5dcd2c78a9c522adc
Vinicius <viniscrv@gmail.com>
2023-09-02 15:43:53 -0300

    test: configurado banco isolado por teste e2e

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -7,6 +7,7 @@ export default defineConfig({
         include: ["**/*.e2e-test.ts"],
         globals: true,
         root: "./",
+        setupFiles: ["./test/setup-e2e.ts"]
     },
     plugins: [
         tsConfigPaths(),
