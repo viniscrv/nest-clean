f598addbff0fdb9387f4895e17fab105e3074a66
Vinícius <viniscrv@gmail.com>
2023-09-08 18:10:51 -0300

    feat: implementado question repository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,8 @@
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { Question } from "@/domain/forum/enterprise/entities/question";
+import { QuestionAttachment } from "@/domain/forum/enterprise/entities/question-attachment";
 import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
-import { Question as PrismaQuestion } from "@prisma/client";
+import { Prisma, Question as PrismaQuestion } from "@prisma/client";
 
 export class PrismaQuestionMapper {
     static toDomain(raw: PrismaQuestion): Question {
@@ -19,5 +20,18 @@ export class PrismaQuestionMapper {
             },
             new UniqueEntityID(raw.id),
         );
+    }
+
+    static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+        return {
+            id: question.id.toString(),
+            authorId: question.authorId.toString(),
+            bestAnswerId: question.bestAnswerId?.toString(),
+            title: question.title,
+            content: question.content,
+            slug: question.slug.value,
+            createdAt: question.createdAt,
+            updatedAt: question.updatedAt,
+        };
     }
 }
