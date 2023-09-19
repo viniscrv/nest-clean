1fa8fc9c7704cc485a9ce2101f55035fd6c977ad
Vinícius <viniscrv@gmail.com>
2023-09-18 22:22:23 -0300

    feat: adicionado controller de remoção de pergunta

---
+++ src/infra/http/controllers/delete-question.controller.e2e-test.ts
@@ -0,0 +1,58 @@
+import { AppModule } from "@/infra/app.module";
+import { DatabaseModule } from "@/infra/database/database.module";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { INestApplication } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Test } from "@nestjs/testing";
+import request from "supertest";
+import { QuestionFactory } from "test/factories/make-question";
+import { StudantFactory } from "test/factories/make-studant";
+
+describe("Delete question (E2E)", () => {
+    let app: INestApplication;
+    let studantFactory: StudantFactory;
+    let questionFactory: QuestionFactory;
+    let prisma: PrismaService;
+    let jwt: JwtService;
+
+    beforeAll(async () => {
+        const moduleRef = await Test.createTestingModule({
+            imports: [AppModule, DatabaseModule],
+        }).compile();
+
+        app = moduleRef.createNestApplication();
+        studantFactory = moduleRef.get(StudantFactory);
+        questionFactory = moduleRef.get(QuestionFactory);
+        prisma = moduleRef.get(PrismaService);
+        jwt = moduleRef.get(JwtService);
+
+        await app.init();
+    });
+
+    test("[DELETE] /questions/:id", async () => {
+        const user = await studantFactory.makePrismaStudant();
+
+        const accessToken = jwt.sign({ sub: user.id.toString() });
+
+        const question = await questionFactory.makePrismaQuestion({
+            authorId: user.id
+        });
+
+        const questionId = question.id.toString();
+
+        const response = await request(app.getHttpServer())
+            .delete(`/questions/${questionId}`)
+            .set("Authorization", `Bearer ${accessToken}`)
+            .send();
+
+        expect(response.statusCode).toBe(204);
+
+        const questionOnDatabase = await prisma.question.findUnique({
+            where: {
+                id: questionId,
+            },
+        });
+
+        expect(questionOnDatabase).toBeNull();
+    });
+});
