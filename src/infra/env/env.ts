4819761f213b6670e22ae72f31726cbe0adf6516
Vinícius <viniscrv@gmail.com>
2023-09-26 21:58:18 -0300

    feat: integração com cloudflare r2

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -3,6 +3,10 @@ import { z } from "zod";
 export const envSchema = z.object({
     DATABASE_URL: z.string().url(),
     JWT_SECRET: z.string(),
+    CLOUDFLARE_ACCOUNT_ID: z.string(),
+    AWS_BUCKET_NAME: z.string(),
+    AWS_ACCESS_KEY_ID: z.string(),
+    AWS_SECRET_ACCESS_KEY: z.string(),
     PORT: z.coerce.number().optional().default(3000),
 });
 
